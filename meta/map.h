4bd8f1b575967a877cbb69e861994f0aef5e688f
Tony Thompson <ajthomps51@gmail.com>
2019-08-21 20:23:37 -0400

    Added ability to find a value given a type

--- meta/map.h
+++ meta/map.h
@@ -15,7 +15,70 @@
 
 #include <type_traits>
 
-// Naive implementation of compile type value to type map.
+/* @brief
+   Container mapping values to types and vice versa, the simple example being string literals to
+   types, such as "int" to int.  Values do not all have to be of the same type. Create using
+   make_map() as shown below. Search for types by value using TSL_META_FIND_TYPE as shown below:
+
+   @code
+   auto constexpr my_map = make_map(
+      make_pair<int>("int")
+      , make_pair<double>("double")
+      , make_pair<unsigned>(1)
+      , make_pair<my_type>("my_type")
+      , make_pair<long>("long"));
+
+   static_assert(std::is_same_v<int,      TSL_META_FIND_TYPE(my_map, "int")>);
+   static_assert(std::is_same_v<double,   TSL_META_FIND_TYPE(my_map, "double")>);
+   static_assert(std::is_same_v<long,     TSL_META_FIND_TYPE(my_map, "long")>);
+   static_assert(std::is_same_v<my_type,  TSL_META_FIND_TYPE(my_map, "my_type")>);
+   static_assert(std::is_same_v<unsigned, TSL_META_FIND_TYPE(my_map, 1)>);
+   @endcode
+
+   You can also do the opposite lookup, give a type and retrieve a value:
+   @code
+   static_assert("int"sv     == my_map.find<int>());
+   static_assert("double"sv  == my_map.find<double>());
+   static_assert(1           == my_map.find<unsigned>());
+   static_assert("my_type"sv == my_map.find<my_type>());
+   static_assert("long"sv    == my_map.find<long>());
+   @endcode
+
+   You can also enumerate all types stored in the map by calling map::enumerate():
+   @code
+   auto constexpr my_map = make_map(
+      make_pair<int>("int")
+      , make_pair<double>("double")
+      , make_pair<unsigned>(1)
+      , make_pair<my_type>("my_type")
+      , make_pair<long>("long"));
+
+   struct my_handler
+   {
+      template <class KeyType, class ValueType>
+      void
+      on_value(ValueType _value)
+      {
+         // ... logic here
+      }
+   };
+
+   my_handler handler;
+   my_map.enumerate(handler); // calls handler<ValueType>(KeyType) for each pair in map.
+
+   @endcode
+
+   @note When searching for types, values are assumed to be unique.  When searching for values,
+   types are assumed to be unique.  If either of these doesn't hold, I make no gaurentees which
+   repeated value/type will be returned.
+ */
+namespace tsl { namespace meta { namespace details {
+   template <class...>
+   struct find_type;
+
+   template <class... Args>
+   using find_type_t = typename find_type<Args...>::type;
+}}}
 
 namespace tsl { namespace meta {
    
@@ -42,52 +105,6 @@ namespace tsl { namespace meta {
       int index;
    };
 
-   /* @brief
-      Container mapping values to types, the simple example being string literals to types, such as
-      "int" to int.  Values do not all have to be of the same type. Create using make_map() as shown
-      below. Search for types by value using TSL_META_FIND_TYPE as shown below:
-
-      @code
-      auto constexpr my_map = make_map(
-         make_pair<int>("int")
-         , make_pair<double>("double")
-         , make_pair<unsigned>(1)
-         , make_pair<my_type>("my_type")
-         , make_pair<long>("long"));
-
-      static_assert(std::is_same_v<int,      TSL_META_FIND_TYPE(my_map, "int")>);
-      static_assert(std::is_same_v<double,   TSL_META_FIND_TYPE(my_map, "double")>);
-      static_assert(std::is_same_v<long,     TSL_META_FIND_TYPE(my_map, "long")>);
-      static_assert(std::is_same_v<my_type,  TSL_META_FIND_TYPE(my_map, "my_type")>);
-      static_assert(std::is_same_v<unsigned, TSL_META_FIND_TYPE(my_map, 1)>);
-      @endcode
-
-      You can also enumerate all types stored in the map by calling map::enumerate():
-      @code
-      auto constexpr my_map = make_map(
-         make_pair<int>("int")
-         , make_pair<double>("double")
-         , make_pair<unsigned>(1)
-         , make_pair<my_type>("my_type")
-         , make_pair<long>("long"));
-
-      struct my_handler
-      {
-         template <class KeyType, class ValueType>
-         void
-         on_value(ValueType _value)
-         {
-            // ... logic here
-         }
-      };
-
-      my_handler handler;
-      my_map.enumerate(handler); // calls handler<ValueType>(KeyType) for each pair in map.
-
-      @end
-
-      @note Types and values are assumed to be unique.
-    */
    template <class...>
    class map;
 
@@ -107,7 +124,7 @@ namespace tsl { namespace meta {
       find(KeyType _key) const;
       
       template <class ValueType>
-      constexpr answer<Pair>
+      constexpr details::find_type_t<ValueType, Pair>
       find() const;
 
    private:
@@ -129,7 +146,7 @@ namespace tsl { namespace meta {
       find(KeyType _key) const;
 
       template <class ValueType>
-      constexpr answer<Pair, Pairs...>
+      constexpr details::find_type_t<ValueType, Pair, Pairs...>
       find() const;
 
       Pair          current;
@@ -140,6 +157,10 @@ namespace tsl { namespace meta {
    constexpr map<Args...>
    make_map(Args... _args);
 
+   // I don't believe I can make this any nicer without resorting to non-type template parameters,
+   // which would limit the types that can be used as keys.  Otherwise I need to wait for consteval
+   // functions, since the constexpr requirement of having to be able to run at runtime limits what
+   // I can do.
 #define TSL_META_FIND_TYPE(_map, _key)                                                             \
    typename tsl::meta::details::extract_type<_map.find(_key).index, decltype(_map)>::type
 }}
@@ -161,6 +182,48 @@ namespace tsl { namespace meta { namespace details {
    {
       using type = typename Head::value;
    };
+
+   // **********************************************************************************************
+   // Notes:
+   // ----------------------------------------------------------------------------------------------
+   template <class ValueType, class Pair>
+   struct find_type<ValueType, Pair>
+   {
+      using type = std::conditional_t
+      <
+         std::is_same_v<typename Pair::value, ValueType>
+         , typename Pair::key_type
+         , void
+      >;
+   };
+
+   template <class ValueType, class Pair, class... Pairs>
+   struct find_type<ValueType, Pair, Pairs...>
+   {
+      static bool const match = std::is_same_v<typename Pair::value, ValueType>;
+
+      using type
+         = std::conditional_t<match, typename Pair::key_type, find_type_t<ValueType, Pairs...>>;
+   };
+   
+   // **********************************************************************************************
+   // Notes:
+   // ----------------------------------------------------------------------------------------------
+   template <class T, class KeyType, class ValueType, class = void>
+   struct is_valid_enumerator : std::false_type
+   {
+
+   };
+
+   template <class T, class KeyType, class ValueType>
+   struct is_valid_enumerator<T, KeyType, ValueType,
+      std::void_t<decltype(
+         std::declval<T>().template on_value<ValueType>(std::declval<KeyType>()))>> : std::true_type
+   {
+   };
+
+   template <class T, class KeyType, class ValueType>
+   inline constexpr bool is_valid_enumerator_v = is_valid_enumerator<T, KeyType, ValueType>::value;
 
    // **********************************************************************************************
    // Notes: For string literals we want to make sure to compare them properly, so we turn them and
@@ -194,7 +257,10 @@ namespace tsl { namespace meta {
    struct pair
    {
       using value = ValueType;
+      using key_type = KeyType;
+
       KeyType key;
+
       constexpr pair(KeyType _key)
          : key(_key)
       {
@@ -231,22 +297,6 @@ namespace tsl { namespace meta {
    {
    }
    
-   template <class T, class KeyType, class ValueType, class = void>
-   struct is_valid_enumerator : std::false_type
-   {
-
-   };
-
-   template <class T, class KeyType, class ValueType>
-   struct is_valid_enumerator<T, KeyType, ValueType,
-      std::void_t<decltype(
-         std::declval<T>().template on_value<ValueType>(std::declval<KeyType>()))>> : std::true_type
-   {
-   };
-
-   template <class T, class KeyType, class ValueType>
-   inline constexpr bool is_valid_enumerator_v = is_valid_enumerator<T, KeyType, ValueType>::value;
-   
    // **********************************************************************************************
    // Notes:
    // ----------------------------------------------------------------------------------------------
@@ -277,6 +327,28 @@ namespace tsl { namespace meta {
       }
       return result;
    }
+
+   // **********************************************************************************************
+   // Notes: We will only use this in a constexpr context, so throwing an exception will be similar
+   // to triggering a static_assert.  We have no way to trigger a static assert here because the
+   // comparison depends on _key.  This is where we would want a "consteval" function so that we
+   // don't have to make this work at runtime as well.
+   // ----------------------------------------------------------------------------------------------
+   template <class Pair>
+   template <class ValueType>
+   constexpr details::find_type_t<ValueType, Pair>
+   map<Pair>::find() const
+   {
+      if constexpr(std::is_same_v<ValueType, typename Pair::value>)
+      {
+         return current.key;
+      }
+      else
+      {
+         throw "Value not found in map";
+      }
+   }
+
    // **********************************************************************************************
    // Notes:
    // ----------------------------------------------------------------------------------------------
@@ -318,5 +390,24 @@ namespace tsl { namespace meta {
       auto retval(others.find(_key));
       decltype(find(_key)) result(retval.index + 1);
       return result;
+   }
+
+   // **********************************************************************************************
+   // Notes:
+   // ----------------------------------------------------------------------------------------------
+   template <class Pair, class... Pairs>
+   template <class ValueType>
+   constexpr details::find_type_t<ValueType, Pair, Pairs...>
+   map<Pair, Pairs...>::find() const
+   {
+      if constexpr(std::is_same_v<ValueType, typename Pair::value>)
+      {
+         return current.key;
+      }
+      else
+      {
+         auto const result(others.template find<ValueType>());
+         return result;
+      }
    }
 }}
--- meta/ut/map_ut.cpp
+++ meta/ut/map_ut.cpp
@@ -43,13 +43,21 @@ TEST_CASE("test tsl::meta::map", "[map]")
       , make_pair<unsigned>(1)
       , make_pair<my_type>("my_type")
       , make_pair<long>("long"));
+   
+   static_assert(std::is_same_v<int, my_map::find("int")>);
 
-   static_assert(std::is_same_v<int,     TSL_META_FIND_TYPE(my_map, "int")>);
-   static_assert(std::is_same_v<double,  TSL_META_FIND_TYPE(my_map, "double")>);
-   static_assert(std::is_same_v<long,    TSL_META_FIND_TYPE(my_map, "long")>);
-   static_assert(std::is_same_v<my_type, TSL_META_FIND_TYPE(my_map, "my_type")>);
+   static_assert(std::is_same_v<int,      TSL_META_FIND_TYPE(my_map, "int")>);
+   static_assert(std::is_same_v<double,   TSL_META_FIND_TYPE(my_map, "double")>);
+   static_assert(std::is_same_v<long,     TSL_META_FIND_TYPE(my_map, "long")>);
+   static_assert(std::is_same_v<my_type,  TSL_META_FIND_TYPE(my_map, "my_type")>);
    static_assert(std::is_same_v<unsigned, TSL_META_FIND_TYPE(my_map, 1)>);
 
    my_handler handler;
    my_map.enumerate(handler);
+   
+   static_assert("int"sv     == my_map.find<int>());
+   static_assert("double"sv  == my_map.find<double>());
+   static_assert(1           == my_map.find<unsigned>());
+   static_assert("my_type"sv == my_map.find<my_type>());
+   static_assert("long"sv    == my_map.find<long>());
 }
